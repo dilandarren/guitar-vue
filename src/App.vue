eeebe98d6220bbdefe2df6f58c21740348489367
Dilan <dilandarren1@gmail.com>
2024-08-23 21:28:43 +0200

    finish proyect

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 
-import {ref, reactive, onMounted} from 'vue'
+import {ref, reactive, onMounted, watch} from 'vue'
 import {db} from './data/guitarras'
 import Guitarra from './components/Guitarra.vue';
 import Header from './components/Header.vue'
@@ -11,11 +11,26 @@ const guitarras = ref([]);
 const carrito = ref([]);
 const guitarra = ref({});
 
+watch(carrito, () => {
+  guardarLocalStorage();
+}, {
+  deep: true
+});
+
 
 onMounted(() => {
   guitarras.value = db;
   guitarra.value = db[3];
+
+  const carritoStorage = localStorage.getItem('carrito')
+  if (carritoStorage) {
+    carrito.value = JSON.parse(carritoStorage);
+  }
 })
+
+const guardarLocalStorage = () => {
+  localStorage.setItem('carrito', JSON.stringify(carrito.value));
+}
 
 
 const agregarCarrito = (guitarra) => {
@@ -26,6 +41,8 @@ const agregarCarrito = (guitarra) => {
       guitarra.cantidad = 1;
       carrito.value.push(guitarra);
     }
+
+    guardarLocalStorage();
     
 }
 
